931330ec8b3102a35f73a5389b5c7b4473d8b659
xoventechdev <alkarim.doha@gmail.com>
2024-07-21 08:26:50 +0300

    added extra QAR

--- content.js
+++ content.js
@@ -1,21 +1,79 @@
 // Function to convert and update prices
 function convertPrices(conversionRate) {
-  const priceElements = document.querySelectorAll(".chakra-text.css-11t9n23");
+  // const priceElements = document.querySelectorAll(".chakra-text.css-11t9n23");
 
-  if (priceElements.length > 0) {
-    for (let element of priceElements) {
-      const bdtText = element.textContent;
-      const bdtAmount = parseFloat(bdtText.replace(/[^0-9.]/g, ""));
-      if (!isNaN(bdtAmount)) {
-        const qarAmount = (bdtAmount / conversionRate).toFixed(2);
-        element.textContent = `${bdtText} # ${qarAmount} QAR`;
-      } else {
-        console.error("Invalid BDT amount found.");
-      }
+  // Website A
+  const priceElementsA = document.querySelectorAll(
+    ".b2b-flight-price--gross-fare-tooltip span"
+  );
+  priceElementsA.forEach((element) => {
+    const bdtText = element.textContent.match(/(\d+(\.\d{1,2})?)\s*BDT/);
+    if (bdtText && bdtText[1]) {
+      const bdtAmount = parseFloat(bdtText[1]);
+      let qarAmount = (bdtAmount * conversionRate).toFixed(2);
+      if (qarAmount < 500) {
+        qarAmount += 20;
+      } else if (qarAmount > 1000) {
+        qarAmount += 10;
+      } else qarAmount += 5;
+      element.textContent = `${bdtText[0]} # ${qarAmount} QAR`;
     }
-  } else {
-    console.error("Ticket prices elements not found.");
-  }
+  });
+
+  // Website B
+  const priceElementsB = document.querySelectorAll(".css-whdouq .chakra-text");
+  priceElementsB.forEach((element) => {
+    const bdtText = element.textContent.match(/(\d+(\.\d{1,2})?)\s*BDT/);
+    if (bdtText && bdtText[1]) {
+      const bdtAmount = parseFloat(bdtText[1]);
+      let qarAmount = (bdtAmount * conversionRate).toFixed(2);
+      if (qarAmount < 500) {
+        qarAmount += 20;
+      } else if (qarAmount > 1000) {
+        qarAmount += 10;
+      } else qarAmount += 5;
+      element.textContent = `${bdtText[0]} # ${qarAmount} QAR`;
+    }
+  });
+
+  // Website C
+  const priceElementsC = document.querySelectorAll(".priceViewer");
+  priceElementsC.forEach((element) => {
+    const bdtText = element.textContent.match(/(\d+(\.\d{1,2})?)/);
+    if (bdtText && bdtText[1]) {
+      const bdtAmount = parseFloat(bdtText[1]);
+      let qarAmount = (bdtAmount * conversionRate).toFixed(2);
+      if (qarAmount < 500) {
+        qarAmount += 20;
+      } else if (qarAmount > 1000) {
+        qarAmount += 10;
+      } else qarAmount += 5;
+      element.textContent = `${element.textContent.trim()} # ${qarAmount} QAR`;
+    }
+  });
+
+  // Website C
+  const priceElementsD = document.querySelectorAll(".css-11t9n23");
+  priceElementsD.forEach((element) => {
+    const bdtText = element.textContent;
+    const bdtAmount = parseFloat(bdtText.replace(/[^0-9.]/g, ""));
+    if (!isNaN(bdtAmount)) {
+      let qarAmount = bdtAmount / conversionRate;
+
+      if (qarAmount < 500) {
+        qarAmount += 20;
+      } else if (qarAmount < 1000) {
+        qarAmount += 10;
+      } else {
+        qarAmount += 5;
+      }
+
+      qarAmount = qarAmount.toFixed(2);
+      element.textContent += ` # ${qarAmount} QAR`;
+    } else {
+      console.error("Invalid BDT amount found.");
+    }
+  });
 }
 
 // Listen for messages from the popup
